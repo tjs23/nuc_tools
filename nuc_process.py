f44b171c3e261daf60678637e6f95d0e31596a33
wb104 <wb104@cam.ac.uk>
2017-11-13 12:32:10 +0000

    Split nuc_util into multiple files

---
+++ nuc_process.py
@@ -0,0 +1,97 @@
+import multiprocessing
+import os
+import subprocess
+
+
+# #   Globals  # #
+
+MAX_CORES        = multiprocessing.cpu_count()
+
+
+# #  Job execution  # #    
+    
+    
+def _parallel_func_wrapper(queue, target_func, proc_data, common_args):
+  
+  for t, data_item in proc_data:
+    result = target_func(data_item, *common_args)
+    
+    if queue:
+      queue.put((t, result))
+  
+
+def parallel_split_job(target_func, split_data, common_args, num_cpu=MAX_CORES, collect_output=True):
+  
+  num_tasks   = len(split_data)
+  num_process = min(num_cpu, num_tasks)
+  processes   = []
+  
+  if collect_output:
+    queue = multiprocessing.Queue() # Queue will collect parallel process output
+  
+  else:
+    queue = None
+    
+  for p in range(num_process):
+    # Task IDs and data for each task
+    # Each process can have multiple tasks if there are more tasks than processes/cpus
+    proc_data = [(t, data_item) for t, data_item in enumerate(split_data) if t % num_cpu == p]
+    args = (queue, target_func, proc_data, common_args)
+
+    proc = multiprocessing.Process(target=_parallel_func_wrapper, args=args)
+    processes.append(proc)
+  
+  for proc in processes:
+    proc.start()
+  
+  
+  if queue:
+    results = [None] * num_tasks
+    
+    for i in range(num_tasks):
+      t, result = queue.get() # Asynchronous fetch output: whichever process completes a task first
+      results[t] = result
+ 
+    queue.close()
+ 
+    return results
+  
+  else:
+    for proc in processes: # Asynchromous wait and no output captured
+      proc.join()
+    
+    
+def call(cmd_args, stdin=None, stdout=None, stderr=None, verbose=True, wait=True, path=None):
+  """
+  Wrapper for external calls to log and report commands,
+  open stdin, stderr and stdout etc.
+  """
+  
+  if verbose:
+    info(' '.join(cmd_args))
+  
+  if path:
+    env = dict(os.environ)
+    prev = env.get('PATH', '')
+    
+    if path not in prev.split(':'):
+      env['PATH'] = prev + ':' + path
+  
+  else:
+    env = None # Current environment variables 
+      
+  if stdin and isinstance(stdin, str):
+    stdin = open(stdin)
+
+  if stdout and isinstance(stdout, str):
+    stdout = open(stdout, 'w')
+
+  if stderr and isinstance(stderr, str):
+    stderr = open(stderr, 'a')
+  
+  if wait:
+    subprocess.call(cmd_args, stdin=stdin, stdout=stdout, stderr=stderr, env=env)
+      
+  else:
+    subprocess.Popen(cmd_args, stdin=stdin, stdout=stdout, stderr=stderr, env=env)
+  
