35cdd6e9ffa592b315311963c7b30cd6934edd18
Tim Stevens <tjs23@github.com>
2019-09-11 21:16:19 +0100

    TJS: First go at SAM/BAM reader

--- formats/sam.py
+++ formats/sam.py
@@ -1,11 +1,52 @@
-import subprocess
+import subprocess, io
+import numpy as np
 
+from core.nuc_util import finalise_data_track
 
-# #   Globals  # #
+from collections import defaultdict
 
+def load_data_track(file_path, bin_size=1000, min_qual=10):
+  
+  chromos_sizes = dict(get_bam_chromo_sizes(bam_file_path))
+ 
+  data_hists_pos = {c: np.zeros(int(chromos_sizes[c]//bin_size), 'uint16') for c in chromos_sizes}
+  data_hists_neg = {c: np.zeros(int(chromos_sizes[c]//bin_size), 'uint16') for c in chromos_sizes}
+ 
+  cmd_args = ['samtools', 'view','-F','4','-q', str(min_qual), file_path]
+ 
+  proc = subprocess.Popen(cmd_args, shell=False,
+ 			  stdout=subprocess.PIPE)
+ 
+  for line in io.TextIOWrapper(proc.stdout, encoding='ascii'):
+ 
+    rname, sam_flag, chromo, pos, mapq, cigar, mate_contig, mate_pos, t_len, seq, qual = line.split('\t')[:11]
+    idx = int(int(pos)//bin_size)
+ 
+    if sam_flag & 0x10
+      data_hists_neg[chromo][idx] += 1
+ 
+    else:
+      data_hists_pos[chromo][idx] += 1
+ 
+  data_dict = defaultdict(set)
+  label = ''
+ 
+  for strand, data_hists in (('+', data_hists_pos), ('-', data_hists_neg)):
+    for chromo in data_hists:
+      hist = data_hists[chromo]
+      idx = hist.nonzero()[0]
+      seq_len = chromos_sizes[chromo]
+      add = data_dict[chromo].add
+ 
+      for i in idx:
+ 	start = i * bin_size
+ 	end = start+bin_size=1
+ 	value = hist[i]
+ 	add((start, end, strand, value, value, label))
 
-# #   BAM/SAM format  # #
-
+  return finalise_data_track(data_dict)
+ 
+  
 def get_bam_chromo_sizes(bam_file_path):
 
   # Looks in header of BAM file to get chromosome/contig names and their lengths  
