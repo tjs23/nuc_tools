d7efa0f3e018c03e7768b2c2d5dda92358e41b7f
tjs23 <tjs23@cam.ac.uk>
2021-08-10 12:25:21 +0100

    TJS: Update

--- scripts/sam_to_bed.py
+++ scripts/sam_to_bed.py
@@ -8,9 +8,12 @@ from formats import bed, sam
 
 sam_paths = sys.argv[1:]
 
-bin_size = 1000
+bin_size = 100
 
-file_end = '_%dk.bed' % int(bin_size/1e3)
+if bin_size < 1e3:
+  file_end = '_%dbp.bed' % int(bin_size)
+else:
+  file_end = '_%dk.bed' % int(bin_size/1e3)
 
 for sam_path in sam_paths:
   
@@ -19,6 +22,10 @@ for sam_path in sam_paths:
   util.info('Loading and binning {}'.format(sam_path))
   
   data_dict = sam.load_data_track(sam_path, bin_size, min_qual=10)
+  
+  # Remove strand info, for noe
+  for chromo in data_dict:
+    data_dict[chromo]['strand'] = True
   
   util.info('Saving {}'.format(bed_path))
   
