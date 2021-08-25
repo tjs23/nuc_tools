769ace3e5255183aceec8d3e127887451888b3c4
tjs23 <tjs23@cam.ac.uk>
2021-08-25 15:06:30 +0100

    TJS: Add min choromo/contig size option for NCC filter

--- tools/ncc_filter.py
+++ tools/ncc_filter.py
@@ -2,17 +2,16 @@ import sys, os
 from collections import defaultdict
 
 PROG_NAME = 'ncc_filter'
-VERSION = '1.0.0'
+VERSION = '1.1.0'
 DESCRIPTION = 'Filter NCC format Hi-C contact files'
 
 def filter_ncc(ncc_in, ncc_out, keep_cis, keep_cis_near, keep_cis_far, keep_trans,
-               keep_ambig, keep_unambig, keep_homolog, keep_nonhomolog, keep_chromos, bp_range):
+               keep_ambig, keep_unambig, keep_homolog, keep_nonhomolog, keep_chromos,
+               bp_range, min_size):
 
   from nuc_tools import util, io
 
-  i = 0
-  j = 0
-  ags_in = defaultdict(int)
+  ag_sizes = defaultdict(int)
   ags_out = set()
   ags_out_add = ags_out.add
   
@@ -31,21 +30,72 @@ def filter_ncc(ncc_in, ncc_out, keep_cis, keep_cis_near, keep_cis_far, keep_tran
   if bp_range:
     bp_min, bp_max = sorted(bp_range)
   
+  min_size *= 1000 # Was Kb
+  max_pos = defaultdict(int)
+  
+  util.info(f'Pre-parsing {ncc_in}')
+  
+  ag = 0
   with io.open_file(ncc_in) as in_file_obj:
-    for line in in_file_obj:
-      ag = line.split()[12]
-      ags_in[ag] += 1      
+    for i, line in enumerate(in_file_obj):
+      chr_a, start_a, end_a, f_start_a, f_end_a, strand_a, \
+        chr_b, start_b, end_b, f_start_b, f_end_b, strand_b, \
+        ambig_group, pair_id, swap_pair = line.split()
+
+      if ambig_group[0] != '0':
+        ag += 1
+        ag_sizes[ag] = int(ambig_group.split('.')[0])     
       
+      if min_size:
+        pos_a = int(f_start_a) if strand_a == '+' else int(f_end_a)
+        pos_b = int(f_start_b) if strand_b == '+' else int(f_end_b)
+ 
+        if pos_a > max_pos[chr_a]:
+          max_pos[chr_a] = pos_a
+
+        if pos_b > max_pos[chr_b]:
+          max_pos[chr_b] = pos_b
+             
+      if i % 100000 == 0:
+         util.info(f' .. {i:,} lines', line_return=True)
+  
+  util.info(f' .. {i:,} lines')
+  
+  util.info(f'Filtering {ncc_in}')
+         
+  i = 0
+  j = 0
+  ag = 0
+  
   with io.open_file(ncc_in) as in_file_obj, open(ncc_out, 'w') as out_file_obj:
     
     write = out_file_obj.write
     
     for line in in_file_obj:
       i += 1
+ 
+      if i % 100000 == 0:
+         util.info(f' .. {i:,} in {j:,} out', line_return=True)
       
       chr_a, start_a, end_a, f_start_a, f_end_a, strand_a, \
         chr_b, start_b, end_b, f_start_b, f_end_b, strand_b, \
         ambig_group, pair_id, swap_pair = line.split()
+     
+      if ambig_group[0] != '0':
+        ag += 1
+      
+      """ E.g.
+      contig_18837 41004 40867 40852 40869 -
+      contig_25335 39867 39792 39766 39794 -
+      1.1 503 1
+      """
+      
+      if min_size:
+        if max_pos[chr_a] < min_size:
+          continue
+
+        if max_pos[chr_b] < min_size:
+          continue
 
       if keep_chromos:
         found_chromos_add(chr_a)
@@ -57,10 +107,10 @@ def filter_ncc(ncc_in, ncc_out, keep_cis, keep_cis_near, keep_cis_far, keep_tran
         if chr_b not in keep_chromos:
           continue
       
-      if keep_unambig and ags_in[ambig_group] > 1:
+      if keep_unambig and ag_sizes[ag] > 1:
         continue
       
-      if keep_ambig and ags_in[ambig_group] < 2:
+      if keep_ambig and ag_sizes[ag] < 2:
         continue
 
       if strand_a == '+':
@@ -88,19 +138,19 @@ def filter_ncc(ncc_in, ncc_out, keep_cis, keep_cis_near, keep_cis_far, keep_tran
           if keep_cis_near and delta <= keep_cis_near:
             j += 1
             write(line)
-            ags_out_add(ambig_group)
+            ags_out_add(ag)
             continue
             
           if keep_cis_far and delta > keep_cis_far:
             j += 1
             write(line)
-            ags_out_add(ambig_group)
+            ags_out_add(ag)
             continue
         
         elif keep_cis:
           j += 1
           write(line)
-          ags_out_add(ambig_group)
+          ags_out_add(ag)
           continue
         
       else:        
@@ -109,21 +159,23 @@ def filter_ncc(ncc_in, ncc_out, keep_cis, keep_cis_near, keep_cis_far, keep_tran
           if chr_a.split('.')[0] == chr_b.split('.')[0]: # chr1.a matches chr1, chr1.b etc
             j += 1
             write(line)
-            ags_out_add(ambig_group)
+            ags_out_add(ag)
             continue
 
         elif keep_nonhomolog:
           if chr_a.split('.')[0] != chr_b.split('.')[0]:
             j += 1
             write(line)
-            ags_out_add(ambig_group)
+            ags_out_add(ag)
             continue         
           
         elif keep_trans:
           j += 1
           write(line)
-          ags_out_add(ambig_group)
+          ags_out_add(ag)
           continue
+  
+  util.info(' .. done')
   
   if keep_chromos:
     unmatched_chromos = keep_chromos - found_chromos
@@ -131,7 +183,7 @@ def filter_ncc(ncc_in, ncc_out, keep_cis, keep_cis_near, keep_cis_far, keep_tran
     if unmatched_chromos:
       util.warn('No contacts found for chromosome(s): %s' % (' '.join(sorted(unmatched_chromos))))
 
-  util.info('Output {:,} lines ({:,} contacts) from input total of {:,} lines ({:,} contacts)'.format(j, len(ags_out), i, len(ags_in)))
+  util.info('Output {:,} lines ({:,} contacts) from input total of {:,} lines ({:,} contacts)'.format(j, len(ags_out), i, len(ag_sizes)))
   
 
 def main(argv=None):
@@ -163,6 +215,9 @@ def main(argv=None):
 
   arg_parse.add_argument('-t', '--keep-trans', default=False, action='store_true', dest='t',
                          help='Include inter-chromosomal (trans) mapping contacts')
+
+  arg_parse.add_argument('-m', '--min-chr-size', type=int, metavar='KB_SEPARATION', dest='m', default=0,
+                         help='Minimum chromosome/contig size, in Kb, for inclusion')
 
   arg_parse.add_argument('-cn', '--keep-cis-near', type=int, metavar='KB_SEPARATION', dest='cn',
                          help='Include near intra-chromosomal (cis) mapping contacts within a specified (kb) sequence separation threshold')
@@ -206,6 +261,7 @@ def main(argv=None):
   keep_nonhomolog  = args['nh'] 
   keep_chromos     = args['chromo']
   bp_range         = args['bp']
+  min_size         = args['m']
   
   if keep_ambig and keep_unambig:
     util.warning('Having both -a and -u options does nothing')
@@ -240,7 +296,8 @@ def main(argv=None):
     keep_trans = True
   
   filter_ncc(ncc_in, ncc_out, keep_cis, keep_cis_near, keep_cis_far, keep_trans,
-             keep_ambig, keep_unambig, keep_homolog, keep_nonhomolog, keep_chromos, bp_range)
+             keep_ambig, keep_unambig, keep_homolog, keep_nonhomolog, keep_chromos,
+             bp_range, min_size)
   
 if __name__ == '__main__':
 
