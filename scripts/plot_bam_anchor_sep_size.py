b71637d5ee5d49e636f6913c5ebde27d569e0a69
tjs23 <tjs23@cam.ac.uk>
2021-10-27 13:20:31 +0100

    TJS: Script improvements

--- scripts/plot_bam_anchor_sep_size.py
+++ scripts/plot_bam_anchor_sep_size.py
@@ -47,7 +47,7 @@ def _load_data_points(data_files):
  
         mids = ((p1 + p2)//2).astype(int)
         mids[s_neg] *= -1 
-        out_dicts[label+'_mid'][chromo] = np.sort(mids)
+        out_dicts[label+'_mid'][chromo] = mids
     
     else:
       out_dicts[label+'_start'] = {}
@@ -66,17 +66,17 @@ def _load_data_points(data_files):
         ends[s_neg] = p1[s_neg]
         ends[s_neg] *= -1
  
-        out_dicts[label+'_start'][chromo] = np.sort(starts)
-        out_dicts[label+'_end'][chromo] = np.sort(ends)
+        out_dicts[label+'_start'][chromo] = starts
+        out_dicts[label+'_end'][chromo] = ends
 
   
   return out_dicts
 
 
-from numba import njit, int64, float64, uint32, prange
+from numba import njit, int64, float64, int32, prange
 
-@njit(int64[:,:](int64[:,:], int64[:,:], int64[:], int64, int64))
-def _add_to_map(regions, data_map, data_points, mid_col, n):
+@njit(int64[:,:](int64[:,:], int64[:,:], int64[:], int64, int64, int64, int64))
+def _add_to_map(regions, data_map, data_points, mid_col, n, min_size, bin_size):
   lim = mid_col-1
   n_regions = len(regions)
   n_points = len(data_points)
@@ -85,7 +85,7 @@ def _add_to_map(regions, data_map, data_points, mid_col, n):
   for r in range(n_regions):
     p1 = regions[r,0]
     p2 = regions[r,1]
-    row = p2-p1
+    row = int((p2-p1-min_size)//bin_size) # Sequence separation
     
     for p in range(p_start, n_points):
       a0 = data_points[p]
@@ -142,13 +142,14 @@ def _add_to_map(regions, data_map, data_points, mid_col, n):
   return data_map
   
 
-def plot_bam_anchor_sep_size(bam_path, anchor_dicts, max_size=500, max_sep=2000, bin_size=10):
+def plot_bam_anchor_sep_size(bam_path, anchor_dicts, min_size=130, max_size=1000, max_sep=2000, bin_size=5):
   
   nx = 2*max_sep + 1
-  ny = max_size
+  ny = int((max_size-min_size)//bin_size)+bin_size
   mid_col = max_sep + 1
   
   anchor_maps = {label:np.zeros((ny, nx), int) for label in anchor_dicts}
+  
   t0 = time.time()
   cmap = util.string_to_colormap('#FFFFFF,#0080FF,#FF0000,#000000')
   
@@ -161,7 +162,7 @@ def plot_bam_anchor_sep_size(bam_path, anchor_dicts, max_size=500, max_sep=2000,
     if i % 100000 == 0:
       util.info('  .. {:,} {:7.2f}s'.format(i, time.time()-t0), line_return=True)
     
-    #if i > 1e7:
+    #if i > 1e6:
     #  break
     
     if chromo == '*':
@@ -190,12 +191,15 @@ def plot_bam_anchor_sep_size(bam_path, anchor_dicts, max_size=500, max_sep=2000,
     if p1 > p2:
       p1, p2, = p2, p1
     
-    size = len(seq) + p2 - p1
+    size = int(t_len) # len(seq) + p2 - p1
     p2 = p1 + size
 
     if size >= max_size:
       continue
-
+    
+    if size < min_size:
+      continue
+    
     chromo_regions[chromo].append((p1, p2))
     
     
@@ -212,35 +216,62 @@ def plot_bam_anchor_sep_size(bam_path, anchor_dicts, max_size=500, max_sep=2000,
         continue
       
       data_points = data_dict[chromo]
+      data_points = data_points[np.abs(data_points).argsort()]
       data_map = anchor_maps[label]
-      _add_to_map(regions, data_map, data_points, mid_col, nx)  
+      _add_to_map(regions, data_map, data_points, mid_col, nx, min_size, bin_size)  
  
   util.info(' Time taken {:7.2f}s'.format(time.time()-t0))
            
-  #kw = {'interpolation':'None', 'norm':norm, 'origin':'lower', 'vmin':v_min, 'vmax':v_max}    
-  matshow_kw = {'norm':None, 'origin':'lower', 'vmin':0.0, 'vmax':1.0}    
-
+  
+  xlabel_pos = np.arange(0, 2*max_sep+1, 500)
+  xlabels = ['%.1f' % ((x-max_sep)*1e-3) for x in xlabel_pos]
+  
+  ystep = 100//bin_size
+  y0  = ystep-divmod(min_size//bin_size, ystep)[1] # Offset to the next 100  
+  ylabel_pos = np.arange(y0, ny, ystep)
+  ylabels = ['%d' % (y*bin_size+min_size) for y in ylabel_pos]
+  
   for label, anchor_map in anchor_maps.items():
     fig, ax = plt.subplots()
+    fig.set_size_inches(8.0, 8.0)
+    matshow_kw = {'norm':None, 'interpolation':'None', 'origin':'lower', 'vmin':0.0, 'vmax':anchor_map.max()}  
+      
     anchor_map = anchor_map.astype(float)
-    anchor_map /= anchor_map.max()
-    ax.matshow(anchor_map, cmap=cmap, aspect='auto')
-    ax.set_title(label)
-    ax.set_xlabel('Separation from anchor')
-    ax.set_ylabel('Fragment size')
+    cax = ax.matshow(anchor_map, cmap=cmap, aspect='auto', **matshow_kw)
     
-    plt.show()
+    ax.tick_params(which='both', direction='out', left=True, right=False, labelright=False, labelleft=True,
+                   labeltop=False, labelbottom=True, top=False, bottom=True, pad=8)
+                     
+    ax.xaxis.set_ticks(xlabel_pos)
+    ax.set_xticklabels(xlabels, fontsize=9)
+    ax.yaxis.set_ticks(ylabel_pos)
+    ax.set_yticklabels(ylabels, fontsize=9)
+
+    ax.set_title('Anchor: ' + label)
+    ax.set_xlabel('Separation from anchor (kb)')
+    ax.set_ylabel('Fragment size (bp)')
+    
+    cbaxes = fig.add_axes([0.92, 0.25, 0.02, 0.5]) # LBWH
+    cbar = plt.colorbar(cax, cax=cbaxes)
+    cbar.ax.tick_params(labelsize=8)
+    cbar.set_label('Read density/max', fontsize=8)
+    
+    save_path = '/data/dino_hi-c/MNase06_anchor_sep_size_' + label + '.pdf'
+    
+    plt.savefig(save_path, dpi=300)
 
 
 if __name__ == '__main__':
    
-  data_files = (('DVNP', '/data/dino_hi-c/ChIP-seq_peaks/DVNP-HiC2_peaks.narrowPeak', 'BED', None, True),
-                ('H2A', '/data/dino_hi-c/ChIP-seq_peaks/H2A-HiC2_peaks.narrowPeak', 'BED', None, True),
+  data_files = (#('DVNP', '/data/dino_hi-c/ChIP-seq_peaks/DVNP-HiC2_peaks.narrowPeak', 'BED', None, True),
+                #('H2A', '/data/dino_hi-c/ChIP-seq_peaks/H2A-HiC2_peaks.narrowPeak', 'BED', None, True),
                 ('Trinity_ssRNA_gene', '/data/dino_hi-c/hem_flye_4_ssRNA_Trinity.gff3', 'GFF', 'gene', False),
-                ('Trinity_ssRNA_exon', '/data/dino_hi-c/hem_flye_4_ssRNA_Trinity.gff3', 'GFF', 'exon', False))
+                #('Trinity_ssRNA_exon', '/data/dino_hi-c/hem_flye_4_ssRNA_Trinity.gff3', 'GFF', 'exon', False),
+                )
   
-  bam_path = '/data/dino_hi-c/Data_tracks_read_align_BAM/MNase-seq/SLX-17948_waller_hem_Mnase-06_sf.bam'
+  bam_path = '/data/dino_hi-c/SLX-17948_waller_hem_Mnase-06_sf.bam'
+  #bam_path = 'SLX-17948_waller_hem_Mnase-06_sf.bam'
     
   anchor_dicts = _load_data_points(data_files)
   
-  plot_bam_anchor_sep_size(bam_path, anchor_dicts, max_size=550, max_sep=2000)   
+  plot_bam_anchor_sep_size(bam_path, anchor_dicts, max_size=800, max_sep=5000)   
--- scripts/plot_hic_anchor_density.py
+++ scripts/plot_hic_anchor_density.py
@@ -10,11 +10,12 @@ from formats import bed, gff, ncc
 def _load_data_points(data_files):
 
   track_data_dicts = {}
-  use_middle = {}
+  file_colors = {}
+  out_styles = {}
   
-  for label, file_path, file_format, feature, is_middle in data_files:
+  for label, colors, file_path, file_format, feature, in data_files:
     util.info('Loading {}'.format(file_path))
-    use_middle[label] = is_middle
+    file_colors[label] = colors
   
     if file_format == 'BED':
       track_data_dicts[label] = bed.load_data_track(file_path)
@@ -26,9 +27,13 @@ def _load_data_points(data_files):
 
   for label in track_data_dicts:
     data_dict = track_data_dicts[label]
-    
-    if use_middle[label]:
-      out_dicts[label+'_mid'] = {}
+    colors = file_colors[label]
+  
+    if len(colors) == 1:
+      key = label+'_mid'
+ 
+      out_dicts[key] = {}
+      out_styles[key] = (colors[0], '-')
 
       for chromo, data_list in data_dict.items():
         p1 = data_list['pos1'].astype(int)
@@ -36,31 +41,37 @@ def _load_data_points(data_files):
         s_neg = data_list['strand'] == 0
  
         mids = ((p1 + p2)//2).astype(int)
-        mids[s_neg] *= -1 
-        out_dicts[label+'_mid'][chromo] = np.sort(mids)
+        mids[s_neg] *= -1
+        out_dicts[key][chromo] = np.sort(mids)
     
     else:
-      out_dicts[label+'_start'] = {}
-      out_dicts[label+'_end']  = {}
+      key1 = label+'_start'
+      key2 = label+'_end'
+      out_dicts[key1] = {}
+      out_dicts[key2]  = {}
+      out_styles[key1] = (colors[0], '-')
+      out_styles[key2] = (colors[1], '-')
 
       for chromo, data_list in data_dict.items():
         p1 = data_list['pos1'].astype(int)
         p2 = data_list['pos2'].astype(int)
         s_neg = data_list['strand'] == 0
  
-        starts = p1[:]
+        starts = p1.copy()
         starts[s_neg] = p2[s_neg]
+        starts_order = starts.argsort()
         starts[s_neg] *= -1
  
-        ends = p2[:]
+        ends = p2.copy()
         ends[s_neg] = p1[s_neg]
+        ends_order = ends.argsort()
         ends[s_neg] *= -1
  
-        out_dicts[label+'_start'][chromo] = np.sort(starts)
-        out_dicts[label+'_end'][chromo] = np.sort(ends)
+        out_dicts[key1][chromo] = starts[starts_order]
+        out_dicts[key2][chromo] = ends[ends_order]
 
   
-  return out_dicts
+  return out_dicts, out_styles
 
 from numba import njit, int64, float64, int32, prange
 
@@ -70,28 +81,41 @@ def _add_to_map(hic_pos, data_map, anchor_points, n_bins, bin_size):
   n_hic = len(hic_pos)
   n_points = len(anchor_points)
   lim = n_bins * bin_size
+  j0 = 0
   
-  for i in range(n_hic):
+  
+  
+  for i in range(n_hic): # Ordered
     p = hic_pos[i]
     
-    for j in range(n_points): # Ordered
+    for j in range(j0, n_points): # Ordered
       a_pos = anchor_points[j]
+      is_neg = a_pos < 0
+      a_pos = abs(a_pos)
       a1 = a_pos-lim
       a2 = a_pos+lim
+      if a2 < p:
+        j0 = j+1
       
       if  a1 < p < a2:
+        delta_bins = (p-a_pos) // bin_size # Negative if hi-c point is before anchor
         
-        k = ((a_pos - p) // bin_size) + n_bins # Zero at n_bins
+        if is_neg:
+          k = n_bins - delta_bins - 1  # Zero/middle at n_bins
+        else:
+          k = n_bins + delta_bins  # Zero/middle at n_bins
+          
         data_map[k] += 1
         
       elif a1 > p:
         break
-  
+      
+      
       
   return data_map
   
   
-def plot_hic_anchor_density(ncc_path, anchor_dicts, max_sep=2000, bin_size=10):
+def plot_hic_anchor_density(ncc_path, anchor_dicts, anchor_styles, max_sep=5000, bin_size=20):
   
   contact_data = defaultdict(list)
   
@@ -104,11 +128,20 @@ def plot_hic_anchor_density(ncc_path, anchor_dicts, max_sep=2000, bin_size=10):
        chr_a, f_start_a, f_end_a, start_a, end_a, strand_a, \
        chr_b, f_start_b, f_end_b, start_b, end_b, strand_b, \
        ambig_code, pair_id, swap_pair = line.split()
-       
-       pos_a = int(f_start_a if strand_a == '+' else f_end_a)
-       pos_b = int(f_start_b if strand_b == '+' else f_end_b)
+       """
+       if strand_a == '+':
+         contact_data[chr_a].append((start_a, f_end_a)
+       else:
+         contact_data[chr_a].append((f_start_a, end_b))
+      
+       if strand_b == '+':
+         contact_data[chr_b].append((start_b, f_end_b)
+       else:
+         contact_data[chr_b].append((f_start_b, end_b))
+       """
+       pos_a = int(f_end_a if strand_a == '+' else f_start_a)
+       pos_b = int(f_end_b if strand_b == '+' else f_start_b)
      
-       contact_data[chr_a].append(pos_a)
        contact_data[chr_b].append(pos_b)
        
        n += 1
@@ -126,36 +159,50 @@ def plot_hic_anchor_density(ncc_path, anchor_dicts, max_sep=2000, bin_size=10):
     
   util.info('Aggregating maps')
   n_bins = max_sep // bin_size
+  n_chromo = len(contact_data)
+  anchor_count = defaultdict(int)
   
-  for chromo in contact_data:
+  for c, chromo in enumerate(contact_data):
     hic_pos = np.array(contact_data[chromo])
+    hic_pos = hic_pos[hic_pos.argsort()]
+    
+    if c and c % 1000 == 0:
+      util.info(f' .. {c:,} of {n_chromo:,}', line_return=True)
     
     for label, data_dict in anchor_dicts.items():
       if chromo not in data_dict:
         continue
       
       anchor_points = data_dict[chromo]
+      anchor_points = anchor_points[np.abs(anchor_points).argsort()]
+      anchor_count[label] += len(anchor_points)
       data_map = anchor_maps[label]
-    
+      
       _add_to_map(hic_pos, data_map, anchor_points, n_bins, bin_size)
+  util.info(f' .. {c:,} of {n_chromo:,}', line_return=True)
   
   fig, ax = plt.subplots()
   fig.set_size_inches(8.0, 8.0)
        
-  ax.set_title('Anchor: ' + label)
-  ax.set_xlabel('Separation from anchor (kb)')
-  ax.set_ylabel('Count')
+  ax.set_title('Anchor Hi-C distribution')
+  ax.set_xlabel('Separation from anchor (bp)')
+  ax.set_ylabel(f'Mean Hi-C contact count ({bin_size} bp bins)')
+  ax.set_xlim((-max_sep, max_sep))
+  ax.set_ylim((0.8, 2.2))
+  
+  x_vals = np.linspace(-max_sep, max_sep, (2*max_sep)//bin_size)
   
   for label, anchor_map in anchor_maps.items():
-     
+    color, linestyle = anchor_styles[label] 
     anchor_map = anchor_map.astype(float)
-  
+    anchor_map /= anchor_count[label]
     
-    ax.plot(anchor_map, label=label, alpha=0.5)
+    ax.plot(x_vals, anchor_map, label=label, color=color, linestyle=linestyle, alpha=0.3)
     
   #save_path = '/data/dino_hi-c/HiC_anchor_sep_distrib_' + label + '.pdf'
   #plt.savefig(save_path, dpi=300)
   
+  ax.axvline(0.0, linestyle='--', linewidth=0.5, alpha=0.5, color='#808080')
   ax.legend(fontsize=8)
   
   plt.show()
@@ -165,14 +212,13 @@ def plot_hic_anchor_density(ncc_path, anchor_dicts, max_sep=2000, bin_size=10):
 
 if __name__ == '__main__':
    
-  data_files = (('DVNP', '/data/dino_hi-c/ChIP-seq_peaks/DVNP-HiC2_peaks.narrowPeak', 'BED', None, True),
-                ('H2A', '/data/dino_hi-c/ChIP-seq_peaks/H2A-HiC2_peaks.narrowPeak', 'BED', None, True),
-                ('Trinity_ssRNA_gene', '/data/dino_hi-c/hem_flye_4_ssRNA_Trinity.gff3', 'GFF', 'gene', False),
-                ('Trinity_ssRNA_exon', '/data/dino_hi-c/hem_flye_4_ssRNA_Trinity.gff3', 'GFF', 'exon', True),
-                ('Trinity_ssRNA_exon', '/data/dino_hi-c/hem_flye_4_ssRNA_Trinity.gff3', 'GFF', 'exon', False)
+  data_files = (('DVNP',               ['#505050',], '/data/dino_hi-c/ChIP-seq_peaks/DVNP-HiC2_peaks.narrowPeak', 'BED', None),
+                ('H2A',                ['#008000',], '/data/dino_hi-c/ChIP-seq_peaks/H2A-HiC2_peaks.narrowPeak', 'BED', None),
+                #('Trin_gene', ['#FF2000','#0080FF',], '/data/dino_hi-c/hem_flye_4_ssRNA_Trinity.gff3', 'GFF', 'gene'),
+                ('Trin_exon', ['#FF2000','#0080FF'], '/data/dino_hi-c/hem_flye_4_ssRNA_Trinity.gff3', 'GFF', 'exon')
                 )
-  anchor_dicts = _load_data_points(data_files)
-  
+  anchor_dicts, anchor_styles = _load_data_points(data_files)
+   
   ncc_path = '/data/dino_hi-c/Hi-C_contact_data/SLX-17943_HEM_FLYE_4_Hi-C.ncc.gz'
  
-  plot_hic_anchor_density(ncc_path, anchor_dicts, max_sep=2000, bin_size=10)
+  plot_hic_anchor_density(ncc_path, anchor_dicts, anchor_styles, max_sep=4000, bin_size=50)
